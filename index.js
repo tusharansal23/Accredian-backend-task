6e52512f117e7ae6210aca3be5ac642b0a4eb709
Tushar <tusharansal23@gmail.com>
2024-07-10 00:17:12 +0530

    Resolved conflicts

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const sendReferralEmail = async (referrerEmail, refereeEmail) => {
   }
 };
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
